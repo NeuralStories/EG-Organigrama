a64a6e457c27997ddab9385360030297c012b744
NeuralStories <ia.hacchi@gmail.com>
2025-09-03 10:25:19 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.jsx';
 
-// 1. Encuentra el elemento del DOM donde se montará la aplicación.
 const rootElement = document.getElementById('root');
-
-// 2. Crea el "root" de React para la aplicación.
 const root = ReactDOM.createRoot(rootElement);
 
-// 3. Renderiza el componente principal <App /> dentro de ese elemento.
-//    StrictMode es una herramienta de React para destacar problemas potenciales.
 root.render(
   <React.StrictMode>
     <App />
